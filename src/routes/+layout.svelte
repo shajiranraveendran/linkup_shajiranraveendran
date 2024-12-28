676dc03337f77e20c5b9160dd2469f9eed97abaf
shajiranraveendran <raveesha@students.zhaw.ch>
2024-12-28 08:40:33 +0100

    ADDED: Layout & Navigation

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,37 @@
+<!-- SCRIPT -->
+<script>
+    import { fly } from "svelte/transition";
+    import "./styles.css";
+    let { children } = $props();
+</script>
+
+<!-- HTML -->
+<!-- NAVIGATION-->
+<div class="nav" transition:fly>
+    <img src="\images\logo.jpg" alt="Logo" class="logo" />
+
+    <nav class="navbar">
+        <div class="container-fluid">
+            <a class="nav-link navfont" href="/">HOME</a>
+            <a class="nav-link navfont" href="/events">EVENTS</a>
+            <a class="nav-link navfont" href="/teilnehmer">TEILNEHMER</a>
+        </div>
+    </nav>
+</div>
+
+<!-- PAGE INHALT-->
+<div>
+    {@render children()}
+</div>
+
+<!-- FOOTER -->
+<footer class="nav footer">@2025 RAVEENDRAN SHAJIRAN - PROTOTYPING</footer>
+
+<!-- STYLE -->
+<style>
+    .logo {
+        height: 50px;
+        margin: 10px 10px;
+        position: absolute;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,43 @@
+<!-- SCRIPT -->
 <script>
-    import "./styles.css";
+    import Video from "$lib/components/Video.svelte";
+    import Page from "./events/+page.svelte";
 </script>
 
+<!-- HTML -->
+<h1>WILLKOMMEN BEI LINKUP!</h1>
+<div class="welcome">
+    <p>
+        Bei LinkUp sind wir überzeugt, dass die besten Verbindungen persönlich
+        entstehen. Unsere Plattform wurde entwickelt, um Gleichgesinnte bei
+        Veranstaltungen zusammenzubringen, die auf deine Interessen
+        zugeschnitten sind. Ob du dein berufliches Netzwerk erweitern oder neue
+        soziale Kontakte knüpfen möchtest, LinkUp bietet dir die Möglichkeit,
+        dich von Angesicht zu Angesicht mit Menschen zu treffen, die deine
+        Leidenschaften teilen.
+    </p>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+    <p>
+        Durchstöbere eine Vielzahl von Events, melde dich an und erlebe die
+        Kraft echter Begegnungen.<br />Es ist an der Zeit, den Bildschirm
+        beiseite zu legen und bedeutungsvolle Beziehungen in der realen Welt zu
+        schaffen.
+    </p>
+
+    <p style="font-weight: bold">
+        WILLKOMMEN IN EINER NEUEN ART DES VERNETZENS – VERLINKE DICH JETZT MIT
+        LINKUP!
+    </p>
+</div>
+
+<!-- VIDEO -->
+<Video />
+
+<!-- STYLE -->
+<style>
+    .welcome {
+        background-color: rgb(5, 32, 43);
+        padding: 20px;
+        border-radius: 5px;
+    }
+</style>
