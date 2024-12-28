67aa1d7f5d781a8b798571cd2022a24aca55859b
shajiranraveendran <raveesha@students.zhaw.ch>
2024-12-28 08:37:25 +0100

    ADDED: CSS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script>
+    import "./styles.css";
+</script>
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
